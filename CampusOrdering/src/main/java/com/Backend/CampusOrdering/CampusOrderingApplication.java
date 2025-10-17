8ec35129d939078d665eea49f843bd1c340e52d4
Bazie521 <MTHBAZ001@myuct.ac.za>
2025-10-17 18:52:21 +0200

    Inital commit: Springboot backend setup

---
+++ CampusOrdering/src/main/java/com/Backend/CampusOrdering/CampusOrderingApplication.java
@@ -0,0 +1,13 @@
+package com.Backend.CampusOrdering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusOrderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusOrderingApplication.class, args);
+	}
+
+}
---
+++ CampusOrdering/src/test/java/com/Backend/CampusOrdering/CampusOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Backend.CampusOrdering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusOrderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
